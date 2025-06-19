52b78337d1cd342c1eba5820179612864916a6ea
izatturganaliev <izatturganaliev@gmail.com>
2025-06-19 16:16:41 -0400

    26 solved

---
+++ src/twopointers/removeduplicatesfromsortedarray_26/Main.java
@@ -0,0 +1,10 @@
+package twopointers.removeduplicatesfromsortedarray_26;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(solution.removeDuplicates(nums));
+    }
+}
---
+++ src/twopointers/removeduplicatesfromsortedarray_26/Solution.java
@@ -0,0 +1,14 @@
+package twopointers.removeduplicatesfromsortedarray_26;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int pos = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[pos]) {
+                nums[++pos] = nums[i];
+            }
+        }
+        return pos + 1;
+    }
+}
