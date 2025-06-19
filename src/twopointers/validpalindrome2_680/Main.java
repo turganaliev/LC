37adecec8903b5faac72df142291e60cbd363fdb
izatturganaliev <izatturganaliev@gmail.com>
2025-06-19 12:34:13 -0400

    680 solved

---
+++ src/twopointers/validpalindrome2_680/Main.java
@@ -0,0 +1,10 @@
+package twopointers.validpalindrome2_680;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String s = "abca";
+        System.out.println(solution.validPalindrome(s));
+    }
+}
---
+++ src/twopointers/validpalindrome2_680/Solution.java
@@ -0,0 +1,27 @@
+package twopointers.validpalindrome2_680;
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0; int r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return isPalindrome(s, l + 1, r) ||
+                        isPalindrome(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
