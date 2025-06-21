5c039a81bd3bb47559959a4bd03e104e5738f851
izatturganaliev <izatturganaliev@gmail.com>
2025-06-20 20:09:19 -0400

    704 solved

---
+++ src/binarysearch/binarysearch_704/Main.java
@@ -0,0 +1,11 @@
+package binarysearch.binarysearch_704;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(solution.search(nums, target));
+    }
+}
