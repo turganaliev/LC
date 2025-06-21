0062f66b58c7cf85d246a6c93babf56b0580ac46
izatturganaliev <izatturganaliev@gmail.com>
2025-06-21 08:26:28 -0400

    35 solved

---
+++ src/binarysearch/searchinsertposition_35/Main.java
@@ -0,0 +1,11 @@
+package binarysearch.searchinsertposition_35;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {1,3,5,6};
+        int target = 2;
+        System.out.println(solution.searchInsert(nums, target));
+    }
+}
