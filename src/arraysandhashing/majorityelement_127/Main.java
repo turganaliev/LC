ca91cfcc35eeb5c17fd0600367a498f86cdd8754
turganaliev <izatturganaliev@gmail.com>
2025-07-13 21:39:32 -0400

    127 done

---
+++ src/arraysandhashing/majorityelement_127/Main.java
@@ -0,0 +1,10 @@
+package arraysandhashing.majorityelement_127;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(solution.majorityElement(nums));
+    }
+}
