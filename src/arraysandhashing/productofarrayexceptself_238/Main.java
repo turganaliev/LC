2e83414127134f4787807bc5effe30eb7405242e
turganaliev <izatturganaliev@gmail.com>
2025-07-13 21:39:47 -0400

    238 done

---
+++ src/arraysandhashing/productofarrayexceptself_238/Main.java
@@ -0,0 +1,12 @@
+package arraysandhashing.productofarrayexceptself_238;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(solution.productExceptSelf(nums)));
+    }
+}
