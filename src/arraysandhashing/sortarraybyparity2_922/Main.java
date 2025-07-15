5aae2dcfcf4637b8e1a81f00fd4749014f4fdb7b
turganaliev <izatturganaliev@gmail.com>
2025-07-14 22:08:08 -0400

    922 done

---
+++ src/arraysandhashing/sortarraybyparity2_922/Main.java
@@ -0,0 +1,12 @@
+package arraysandhashing.sortarraybyparity2_922;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {3,1,4,2};
+        System.out.println(Arrays.toString(solution.sortArrayByParityII(nums)));
+    }
+}
