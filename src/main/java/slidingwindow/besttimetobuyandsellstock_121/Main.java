9b9573e2e03ad13714c839c8de8caebd65392371
turganaliev <izatturganaliev@gmail.com>
2026-04-16 17:35:01 +0600

    initial commit

---
+++ src/main/java/slidingwindow/besttimetobuyandsellstock_121/Main.java
@@ -0,0 +1,8 @@
+package slidingwindow.besttimetobuyandsellstock_121;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+}
