cc4cdbd8e55dc43dc21bee8a2bba9db7e37252f3
turganaliev <izatturganaliev@gmail.com>
2026-04-16 18:34:32 +0600

    done 242

---
+++ src/main/java/arraysandhashing/validanagram_242/Main.java
@@ -0,0 +1,10 @@
+package arraysandhashing.validanagram_242;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isAnagram("anagram", "nagaram"));
+        System.out.println(solution.isAnagram("car", "rat"));
+
+    }
+}
