13ea2a88dec8b826c9c1c1f3858ad1259c361baf
turganaliev <izatturganaliev@gmail.com>
2026-04-16 18:45:52 +0600

    done 191

---
+++ src/main/java/bitmanipulation/numberof1bits_191/Main.java
@@ -0,0 +1,10 @@
+package bitmanipulation.numberof1bits_191;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.hammingWeight(11));
+        System.out.println(solution.hammingWeight(128));
+
+    }
+}
