1f6b284764ff0f6fccf379e2908376c78f7498de
turganaliev <izatturganaliev@gmail.com>
2026-05-06 10:04:59 +0600

    done 75

---
+++ src/main/java/arraysandhashing/sortcolors_75/Main.java
@@ -0,0 +1,10 @@
+package arraysandhashing.sortcolors_75;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.sortColors(new int[]{2,0,2,1,1,0})));
+    }
+}
