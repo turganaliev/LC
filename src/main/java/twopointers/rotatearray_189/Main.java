32801235fc5f0b46dadc577fe9c5beaf1a07c166
turganaliev <izatturganaliev@gmail.com>
2026-05-06 07:10:17 +0600

    done 189

---
+++ src/main/java/twopointers/rotatearray_189/Main.java
@@ -0,0 +1,10 @@
+package twopointers.rotatearray_189;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+    }
+}
