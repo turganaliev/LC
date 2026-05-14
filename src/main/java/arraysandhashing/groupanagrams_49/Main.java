91e5862298d31b1e552c05ddaa4825ec7739c74f
turganaliev <izatturganaliev@gmail.com>
2026-05-14 16:33:12 +0600

    done 49

---
+++ src/main/java/arraysandhashing/groupanagrams_49/Main.java
@@ -0,0 +1,8 @@
+package arraysandhashing.groupanagrams_49;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+    }
+}
