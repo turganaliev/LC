88aee1c01d7d05b4648418f793e6a2d0a3ec6b5d
turganaliev <izatturganaliev@gmail.com>
2026-05-18 07:54:32 +0600

    done 1011

---
+++ src/main/java/binarysearch/capacitytoshippackageswithinddays_1011/Main.java
@@ -0,0 +1,8 @@
+package binarysearch.capacitytoshippackageswithinddays_1011;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.shipWithinDays(new int[]{1,2,3,4,5,6,7,8,9,10}, 5));
+    }
+}
