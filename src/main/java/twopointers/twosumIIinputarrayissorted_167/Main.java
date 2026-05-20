bf7dbbdd357495071e12923352cf220cad4e04b3
turganaliev <izatturganaliev@gmail.com>
2026-05-20 12:56:56 +0600

    done 167

---
+++ src/main/java/twopointers/twosumIIinputarrayissorted_167/Main.java
@@ -0,0 +1,10 @@
+package twopointers.twosumIIinputarrayissorted_167;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] main) {
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+}
