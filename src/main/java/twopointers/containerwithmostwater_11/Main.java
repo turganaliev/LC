7e4ddd2168f2533ddb339b263b9de51613c89007
turganaliev <izatturganaliev@gmail.com>
2026-05-21 10:48:16 +0600

    done 11

---
+++ src/main/java/twopointers/containerwithmostwater_11/Main.java
@@ -0,0 +1,8 @@
+package twopointers.containerwithmostwater_11;
+
+public class Main {
+    public static void main(String[] main) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
+}
