abcdb43139614fcfaf80621f9df294c0d4c06798
turganaliev <izatturganaliev@gmail.com>
2026-05-22 10:12:22 +0600

    done 42

---
+++ src/main/java/twopointers/trappingrainwater_42/Main.java
@@ -0,0 +1,8 @@
+package twopointers.trappingrainwater_42;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+    }
+}
