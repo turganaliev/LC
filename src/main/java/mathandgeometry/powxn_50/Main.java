d3ecbf962762cef88f891c8aac136f4530ea3c83
turganaliev <izatturganaliev@gmail.com>
2026-05-23 07:55:45 +0600

    done 50

---
+++ src/main/java/mathandgeometry/powxn_50/Main.java
@@ -0,0 +1,8 @@
+package mathandgeometry.powxn_50;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.myPow(2.1, 3));
+    }
+}
