c02b944327346cbb8b7352fe565f4cd3d4f5566e
turganaliev <izatturganaliev@gmail.com>
2026-05-24 08:55:01 +0600

    done 77

---
+++ src/main/java/backtracking/combinations_77/Main.java
@@ -0,0 +1,8 @@
+package backtracking.combinations_77;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.combine(4, 2));
+    }
+}
