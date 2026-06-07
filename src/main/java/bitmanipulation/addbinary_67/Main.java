fff2a2434f06954bb5ad033364e163dc437da56b
turganaliev <izatturganaliev@gmail.com>
2026-06-07 20:00:46 +0600

    done 67

---
+++ src/main/java/bitmanipulation/addbinary_67/Main.java
@@ -0,0 +1,8 @@
+package bitmanipulation.addbinary_67;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.addBinary("1010", "1011"));
+    }
+}
