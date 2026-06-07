4695428ce874d0fcd282f880e13931f2336dc1f3
turganaliev <izatturganaliev@gmail.com>
2026-06-07 15:26:53 +0600

    done 27

---
+++ src/main/java/arraysandhashing/removeelement_27/Main.java
@@ -0,0 +1,8 @@
+package arraysandhashing.removeelement_27;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.removeElement(new int[]{0,1,2,2,3,0,4,2}, 2));
+    }
+}
