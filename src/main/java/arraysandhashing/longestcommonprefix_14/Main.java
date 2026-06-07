bc66b4cd1f93d01f010c74e21c4b2f970619bb1f
turganaliev <izatturganaliev@gmail.com>
2026-06-07 19:54:53 +0600

    done 14

---
+++ src/main/java/arraysandhashing/longestcommonprefix_14/Main.java
@@ -0,0 +1,8 @@
+package arraysandhashing.longestcommonprefix_14;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.longestCommonPrefix(new String[]{"flower","flow","flight"}));
+    }
+}
