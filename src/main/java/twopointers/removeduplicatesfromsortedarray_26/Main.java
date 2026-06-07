58c301fd5683309bdf3d19adac5546c3d95defaf
turganaliev <izatturganaliev@gmail.com>
2026-06-07 15:23:08 +0600

    done 26

---
+++ src/main/java/twopointers/removeduplicatesfromsortedarray_26/Main.java
@@ -0,0 +1,8 @@
+package twopointers.removeduplicatesfromsortedarray_26;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+    }
+}
