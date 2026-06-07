7c7eb59e9ff919cb4f43f5521aad66b8d8f7e7f0
turganaliev <izatturganaliev@gmail.com>
2026-06-07 15:29:51 +0600

    done 190

---
+++ src/main/java/bitmanipulation/reversebits_190/Main.java
@@ -0,0 +1,8 @@
+package bitmanipulation.reversebits_190;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.reverseBits(43261596));
+    }
+}
