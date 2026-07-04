7d1eefc625d32d7b91a12168866028d8aeb19c6d
turganaliev <izatturganaliev@gmail.com>
2026-07-04 15:07:49 +0600

    done 21

---
+++ src/main/java/linkedlist/mergetwosortedlists_21/Main.java
@@ -0,0 +1,17 @@
+package linkedlist.mergetwosortedlists_21;
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        Solution solution = new Solution();
+        ListNode head = solution.mergeTwoLists(l1, l2);
+
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+}
