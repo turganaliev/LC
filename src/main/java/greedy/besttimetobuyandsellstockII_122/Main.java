dab8d50e556e76f166d3bfd5dba7ed6b1b209482
turganaliev <izatturganaliev@gmail.com>
2026-07-04 23:05:26 +0600

    done 122

---
+++ src/main/java/greedy/besttimetobuyandsellstockII_122/Main.java
@@ -0,0 +1,8 @@
+package greedy.besttimetobuyandsellstockII_122;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+}
